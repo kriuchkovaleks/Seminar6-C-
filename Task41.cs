2fb4c8f0768a4499d48710e7da17d4eb3ad4ef54
kriuchkovaleks <103981431+kriuchkovaleks@users.noreply.github.com>
2022-05-31 17:25:24 +0300

    Add files via upload

---
+++ Task41.cs
@@ -0,0 +1,28 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+
+
+int AmountOfPositiveNumbers(int amount)
+{
+    int [] array = new int [amount];
+    int summ = 0; 
+    for (int i = 0; i < amount; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        array [i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0)
+            summ = summ + 1;
+        
+    }
+    return summ; 
+}
+
+
+Console.WriteLine("Какое количество цифр Вы хотите ввести");
+int amountOfNumbers = Convert.ToInt32(Console.ReadLine());
+
+int result = AmountOfPositiveNumbers(amountOfNumbers);
+
+Console.WriteLine($"Колличество положительных чисел: {result}");
---
+++ Task43.cs
@@ -0,0 +1,36 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)*/
+
+
+
+void crossPoint(double b1, double b2, double k1, double k2)
+{
+    double y = 0;
+    double x = 0;
+
+    if ((k2 - k1) == 0 || (k1 - k2) == 0)
+    {
+        Console.WriteLine("Решения не существует, прямыe параллельны");
+    }
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;
+        Console.WriteLine($"({x} ; {y})");
+    }
+
+}
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+crossPoint(b1, b2, k1, k2);
+
